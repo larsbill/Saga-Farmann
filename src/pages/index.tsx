190947b8d3d83b919f11cd684094639900e1b716
mobak88 <mostafa0401@ymail.com>
2024-05-28 16:55:42 +0200

    Creating copy of main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,6 +46,7 @@ const Home = ({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* Comment */}
       <Hero />
       <div className={styles["grid-wrapper"]}>
         <GridImagesAndText gridContent={gridSection} />
