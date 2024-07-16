38f1f40834b1dfb06747c2ba7c8ca66b45d893dc
Mustafa Baker <78964465+mobak88@users.noreply.github.com>
2024-07-16 12:42:25 +0200

    Update links.ts

--- src/components/navigation/hamburger/links.ts
+++ src/components/navigation/hamburger/links.ts
@@ -1,7 +1,6 @@
 export const links = (crewId: string) => {
   return [
     { href: "/", label: "Home" },
-    { href: `/crew_members/${crewId}`, label: "Crew" },
     { href: "/sponsors", label: "Sponsors" },
     { href: "/donate", label: "Donate" },
     { href: "/about-us", label: "About us" },
